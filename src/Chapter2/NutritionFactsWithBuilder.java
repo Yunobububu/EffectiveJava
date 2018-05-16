92177219afffb43a5171c92ce7d6454235b408e5
Mikasa <lvjinkebit@163.com>
2018-05-16 16:28:00 +0800

    Builder

---
+++ src/Chapter2/NutritionFactsWithBuilder.java
@@ -0,0 +1,88 @@
+package Chapter2;
+
+public class NutritionFactsWithBuilder {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder{
+        private  int servingSize;
+        private  int servings;
+        private  int calories = 0;
+        private  int fat = 0;
+        private  int sodium = 0;
+        private  int carbohydrate = 0;
+
+        public Builder(int servingSize,int servings){
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val){
+            calories = val;
+            return this;
+        }
+        public Builder fat(int val){
+            fat = fat;
+            return this;
+        }
+        public Builder sodium(int val){
+            sodium = val;
+            return this;
+        }
+        public Builder carbohydrate(int val){
+            carbohydrate = val;
+            return this;
+        }
+        public NutritionFactsWithBuilder build(){
+            return new NutritionFactsWithBuilder(this);
+        }
+    }
+    public NutritionFactsWithBuilder(Builder builder){
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+
+    public int getServingSize() {
+        return servingSize;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public int getSodium() {
+        return sodium;
+    }
+
+    public int getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    @Override
+    public String toString() {
+        return "NutritionFactsWithBuilder{" +
+                "servingSize=" + servingSize +
+                ", servings=" + servings +
+                ", calories=" + calories +
+                ", fat=" + fat +
+                ", sodium=" + sodium +
+                ", carbohydrate=" + carbohydrate +
+                '}';
+    }
+}
